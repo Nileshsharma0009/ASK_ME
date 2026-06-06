fb6ac7ab271e8774ad063294a475c099661d868d
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-06-06 16:40:08 +0530

    updated config , vercel json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   server: {
+//     port: 5173,
+//     open: true,
+//   },
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    // VITE USES POSTCSS.CONFIG.JS AUTOMATICALLY. 
+    // No need to add extra PostCSS plugins here, as they are defined in that file.
   ],
-  server: {
-    port: 5173,
-    open: true,
-  },
 })
