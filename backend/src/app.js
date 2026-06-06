2d5d154bb74422d1043639c98b7795dda7ca344f
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-06-06 17:46:11 +0530

    cors and cold start fixed .raedy to deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,16 +10,41 @@ dotenv.config();
 const app = express() ;
  
 //  TODO : after deployment  dont forget to add  funtion  for cold start on render 
+const url = `https://ask-me-pndw.onrender.com`;
+const interval = 30000;
 
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      // console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
 
 
 
 const baseUrl = process.env.FRONTEND_URL; //  YEH SAHI HAI 
 // todo cors 
-app.use(cors({
-  origin: "https://ask-me-red-xi.vercel.app/"|| "http://localhost:5173",
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: "https://ask-me-red-xi.vercel.app/"|| "http://localhost:5173",
+//   credentials: true,
+// }));
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ask-me-red-xi.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 // app.use (cookieParser()) ;
 
