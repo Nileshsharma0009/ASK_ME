4cceceae5fb7d28ef6d0ea7edaea4411abae48dc
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-06-06 16:09:28 +0530

    c caspitl changed

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { register, login ,muteCompliance } from "../controllers/auth.Controller.js";
-import { updateSettings } from '../controllers/auth.Controller.js';
+import { updateSettings } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
